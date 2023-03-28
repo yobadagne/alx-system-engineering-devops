45535973bafd6093c09705e181fa215612a480e1
yobadagne <yobadagne@gmail.com>
2023-03-28 22:36:42 +0000

    5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,35 @@
+# 6. Install Nginx web server (w/ Puppet)
+exec { 'exec_1':
+  command => 'sudo apt-get update -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_2':
+  require => Exec['exec_1'],
+  command => 'sudo apt-get install nginx -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_3':
+  require => Exec['exec_2'],
+  command => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_4':
+  require     => Exec['exec_3'],
+  environment => ['GG=https://tecnoayuda.co permanent'],
+  command    => 'sudo sed -i "30i \\\trewrite ^/redirect_me $GG;" /etc/nginx/sites-available/default',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0,1]
+}
+
+exec { 'exec_5':
+  require => Exec['exec_4'],
+  command => 'sudo service nginx start',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
+}
