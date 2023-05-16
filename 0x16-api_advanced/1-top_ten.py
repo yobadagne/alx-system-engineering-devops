95b512ad4e4c48e40a80ef03cb366277e4123c33
yobadagne <yobadagne@gmail.com>
2023-05-16 10:23:52 +0000

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,11 +7,9 @@ def top_ten(subreddit):
     """ returns top ten hot posts """
     headers = {"User-Agent": "yobadagne"}
     try:
-        result = get("https://www.reddit.com/r/{}.json?limit=10&sort=hot"
-                      .format(subreddit), headers=headers,
-                      allow_redirects=False).json()
-        children  = result.get("data").get("children")
-        for i in children:
+        result = get("https://www.reddit.com/r/{}.json?limit=10&sort=hot".format(subreddit), headers=headers, allow_redirects=False).json()
+        childrens  = result.get("data").get("children")
+        for i in childrens:
             print(i.get("data").get("title"))
     except:
         print(None)
